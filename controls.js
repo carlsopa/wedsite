063b8587420903db586ff269ea5d6a386441323f
Paul R. Carlson <carlsonpar@gmail.com>
2021-04-11 10:51:44 -0400

    working on creating the actions to open and close the menu on click.

--- controls.js
+++ controls.js
@@ -1,6 +1,8 @@
+console.log('help')
 var menu = document.getElementById("menu-list");
 var menubtn = document.getElementById("menu-btn");
 var menulnk = document.getElementsByClassName("menu-link");
+console.log(menu)
 console.log(menulnk);
 for (var i = 0; i < menulnk.length; i++) {
   menulnk[i].addEventListener("click", (e) => {
